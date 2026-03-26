25b206ef1cbb705d48d7e3ef494a150883edb597
iluvvac <digitaldastin@gmail.com>
2026-03-26 16:18:52 +0700

    change name opengraph-image.jpg

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     siteName: 'Dasteen',
     images: [
       {
-        url: absoluteUrl('/images/metadata_cover_compressed.jpg'),
+        url: absoluteUrl('/images/opengraph-image.jpg'),
         width: 1200,
         height: 628
       }
